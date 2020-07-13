4ec48b57d4ed6e49b4aff230577bab359a2cc90b
aecof <arthurecoffet@gmail.com>
2020-07-13 23:32:17 +0200

    updated simulation file

--- Code/simulation_convolutional.py
+++ Code/simulation_convolutional.py
@@ -19,11 +19,19 @@ weights = [s/frames for s in range(frames)]
 weights = [(elm,1-elm) for elm in weights]
 print(weights)
 for cgpu in ['cpu','gpu'] : 
-    for downsample in [1,2,3] : 
-        for niter in [20, 50, 100, 200] : 
+    for niter in [20, 50, 100, 200] : 
+        for downsample in [1,2,3,4] : 
+        
+
             t0 = time.time()
             tmp1 = _3los[::downsample,::downsample,::downsample]
             tmp2 = _3iyf[::downsample,::downsample,::downsample]
+            file_name = './time_results/%s_%s_niter=%i.txt'%(cgpu,tmp1.shape,niter)
+            isFile = os.path.isfile(file_name) 
+            if isFile : 
+                print('stopping')
+                continue
+            
             for i in range(10) : 
 
                 for weight in weights :
