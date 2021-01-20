8335eb96d332358995f6d5207d0b31a10aba2a4f
rtviii <rtkushner@gmail.com>
2021-01-20 09:02:05 -0800

    Time results, cupy filter

--- otmorph-bundle/src/utils.py
+++ otmorph-bundle/src/utils.py
@@ -1,14 +1,15 @@
 import numpy as np
-from scipy.ndimage import gaussian_filter, gaussian_filter1d
-from scipy.optimize import newton
-import multiprocessing as mp
+from scipy.optimize import newton 
+from scipy.ndimage import gaussian_filter 
+import mrcfile
 import time
-try : 
-    import cupy as cp
-except : 
-    pass
+import sys
+import cupy as cp
+try:
+    from cupyx.scipy.ndimage import gaussian_filter as cpx_gaussian_filter
+except:
+    print("Failed to import cupyx.scipy.ndimage.gaussian_filter (Made available in 9.0.0a3)")
 
-#import mrcfile
 
 
 ###################################################################################################################################
@@ -46,6 +47,67 @@ def compute_from_shape_names(list_shape_names,N, weights):
     return barycenter
 
     
+    
+###################################################################################################################################
+###################################################################################################################################
+################################## MAIN TRANSPORT MAPS FUNCTIONS ##################################################################
+###################################################################################################################################
+###################################################################################################################################
+
+def sinkhorn(mu0, mu1, reg, stabThresh = 1e-30, niter = 100) : 
+    
+    def K(x):
+        
+        return gaussian_filter(x,sigma=reg)
+    
+    
+    mu0 = mu0 / mu0.sum()
+    mu1 = mu1 / mu1.sum()
+    
+    if mu0.shape != mu1.shape :
+        raise ValueError('Shapes are not the same')
+    
+    v = np.ones(mu0.shape)
+    w = np.ones(mu1.shape)
+    
+    for i in range(niter):
+        v = mu0 / np.maximum(stabThresh,K(w) )
+        w = mu1 / np.maximum(stabThresh,K(v) ) 
+        
+        
+    
+    return v,w
+
+
+    
+
+def wasserstein_distance(mu0, mu1, reg, stabThresh = 1e-30, niter = 100) : 
+    
+    def K(x):
+        
+        return gaussian_filter(x,sigma=reg)
+    
+    
+    mu0 = mu0 / mu0.sum()
+    mu1 = mu1 / mu1.sum()
+    area_weights = np.ones(mu0.shape)/mu0.size
+    if mu0.shape != mu1.shape :
+        raise ValueError('Shapes are not the same')
+    
+    v = np.ones(mu0.shape)
+    w = np.ones(mu1.shape)
+    
+    for i in range(niter):
+        v = mu0 / np.maximum(stabThresh,K(w) )
+        w = mu1 / np.maximum(stabThresh,K(v) ) 
+        
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+    
+    # return np.sqrt(reg) * (mu0*np.log(np.maximum(v,1e-30)) + mu1*np.log(np.maximum(w,1e-30))).mean()
+    # Not using the above formula because the regularization term is a function of the 
+    # parameter in gaussian blurr, we don't precisely know what it is so we use a 
+    # formula only based on the gaussian blurr
+    return (v*np.log(np.maximum(v,1e-60))*K(w) + v * K(w*np.log(np.maximum(w,1e-60)))).mean()
 ###################################################################################################################################
 ###################################################################################################################################
 ################################## MAIN BARYCENTER FUNCTIONS ######################################################################
@@ -54,7 +116,32 @@ def compute_from_shape_names(list_shape_names,N, weights):
 
 # ----------------------------------------------------------------------------
 #
-def convolutional_barycenter(Hv,reg,alpha,stabThresh=1e-30,niter=1500,tol=1e-9,sharpening=False, verbose = False, force_cpu =False):
+def linear_barycenter(Hv,alpha):
+    """[summary]
+
+    Args:
+        Hv {(ndarray)} -- Set of distributions 
+        alpha {list} -- set of weights
+
+    Raises:
+        MemoryError: [description]
+
+    Returns:
+        ndarray -- solution of weighted wassertein barycenter problem
+    """
+    for i in range(len(Hv)):
+        Hv[i] = (Hv[i]-Hv[i].min())/Hv[i].sum()
+
+    bary = np.empty(Hv[0].shape)
+    for i in range(len(Hv)) :
+        bary += alpha[i]*Hv[i]
+
+    return bary
+    
+
+# ----------------------------------------------------------------------------
+#
+def convolutional_barycenter(Hv,reg,alpha,stabThresh=1e-30,niter=1500,tol=1e-9,sharpening=False, verbose = False):
     """Calls convolutional barycenter function depending on whether or not cuda gpus are available
 
     Arguments:
@@ -74,16 +161,11 @@ def convolutional_barycenter(Hv,reg,alpha,stabThresh=1e-30,niter=1500,tol=1e-9,s
     """
     try : 
         import cupy
-        #print('cupy is here, running on gpu')
         is_gpu = True
     except :
-        #print('cupy not installed / no cuda GPU on computer, running on cpu')
-        is_gpu = False 
-    
-    if force_cpu :
+        print('cupy not installed / no cuda GPU on computer, running on cpu')
         is_gpu = False 
         
-    #print(alpha)
     if is_gpu :
         return convolutional_barycenter_gpu(Hv,reg,alpha,stabThresh,niter,tol,sharpening, verbose)
     else : 
@@ -133,15 +215,16 @@ def convolutional_barycenter_cpu(Hv, reg, alpha, stabThresh = 1e-30, niter = 150
     barycenter = np.zeros(Hv[0].shape)
     
     change = 1
+    
     for j in range(niter):
         t0 = time.time()
         barycenterOld = barycenter
         
-        barycenter = np.zeros_like(Hv[0, :, :])
+        barycenter = np.zeros_like(Hv[0,:,:])
         for i in range(Hv.shape[0]):
 
             
-            Kw[i,:,:] = K(Hv[i, :, :] / np.maximum(stabThresh,K(v[i, :, :])) )
+            Kw[i,:,:] = K(Hv[i,:,:] / np.maximum(stabThresh,K(v[i, :, :])) )
             
             
             barycenter += alpha[i] * np.log(np.maximum(stabThresh, v[i, :, :]*Kw[i, :, :]))
@@ -161,7 +244,7 @@ def convolutional_barycenter_cpu(Hv, reg, alpha, stabThresh = 1e-30, niter = 150
             barycenter = barycenter**beta
         
         for i in range(Hv.shape[0]):
-            v[i, :, :] =  barycenter / np.maximum(stabThresh, Kw[i, :, :])
+            v[i, :, :] =  barycenter / np.maximum(stabThresh, Kw[i, : ,: ])
             
         if verbose : 
             print("iter : ",j , "change : ", change, "time :" , time.time()-t0)
@@ -170,13 +253,13 @@ def convolutional_barycenter_cpu(Hv, reg, alpha, stabThresh = 1e-30, niter = 150
         if change<tol :
             break
 
-    return barycenter*mean_weights
+    return barycenter
 
 # ----------------------------------------------------------------------------
 #
 def convolutional_barycenter_gpu(Hv,reg,alpha,stabThresh = 1e-30,niter = 1500, tol = 1e-9,sharpening = False,verbose = False):
-    """Main function solving wasserstein barycenter problem using gpu
 
+    """Main function solving wasserstein barycenter problem using gpu
     Arguments:
         Hv {Set of distributions (cparray)} -- 
         reg {regularization term "gamma"} -- float superior to 0, generally equals size of space/40
@@ -188,13 +271,12 @@ def convolutional_barycenter_gpu(Hv,reg,alpha,stabThresh = 1e-30,niter = 1500, t
         tol {float} -- convergence tolerance at which point iterations stop (default: {1e-9})
         sharpening {bool} -- Whether or not entropic sharpening is used (default: {False})
         verbose {bool} --  verbose option
-
     Returns:
         cparray -- solution of weighted wassertein barycenter problem
     """
-    
+
     def K(x):
-        return cp.array(gaussian_filter(cp.asnumpy(x),sigma=reg))
+        return cpx_gaussian_filter(x, sigma=reg)
     
     def to_find_root(barycenter, H0, beta):
         return entropy(barycenter**beta) - H0
@@ -203,6 +285,7 @@ def convolutional_barycenter_gpu(Hv,reg,alpha,stabThresh = 1e-30,niter = 1500, t
     alpha = alpha/alpha.sum()
     Hv = cp.array(Hv)
     mean_weights = (Hv[0].sum()+Hv[1].sum())/2.
+    #print('mean weights', mean_weights)
     for i in range(len(Hv)):
         Hv[i] = Hv[i]/Hv[i].sum()
     v = cp.ones(Hv.shape)
@@ -241,12 +324,12 @@ def convolutional_barycenter_gpu(Hv,reg,alpha,stabThresh = 1e-30,niter = 1500, t
         
 
         if verbose :
+            #sys.stdout('output.log','a')
             print("iter : ",j , "change : ", change, 'time :', time.time()-t0)
         if change<tol :
             break
-    
-    
-    return cp.asnumpy(barycenter*mean_weights)
+
+    return cp.asnumpy(barycenter)
 
 
 
@@ -395,9 +478,9 @@ def load_volume(N,shape):
             
             
         elif shape == 'cubes':
-            r = .15
-            c1 = np.array([.5, .5, .25])
-            c2 = np.array([.5, .5, .75])
+            r = .225
+            c1 = np.array([.5, .5, .225])
+            c2 = np.array([.5, .5, .775])
             cube1 = (abs(xc-c1[0]) < r) & (abs(yc-c1[1]) < r) & (abs(zc-c1[2]) < r)
             cube2 = (abs(xc-c2[0]) < r) & (abs(yc-c2[1]) < r) & (abs(zc-c2[2]) < r)
             res_shape = cube1 + cube2
@@ -440,7 +523,7 @@ def load_volume(N,shape):
             
         
         
-        return (x,y,z),res_shape
+        return res_shape
     
 # ----------------------------------------------------------------------------
 #
@@ -458,104 +541,3 @@ def midpoints(x):
 def normalize(array):
     return array/array.sum()
 
-####################################################################################################################################################
-####################################################################################################################################################
-################################# GAUSSIAN FILTER WITHOUT SCIPY (BASED ON VOLUME FILTER EXTENXION CHIMERA)##########################################
-####################################################################################################################################################
-####################################################################################################################################################
-
-
-# ----------------------------------------------------------------------------
-#
-def ceil_power_of_2(n):
-    """[summary]
-
-    Arguments:
-        n {integer} -- power of 2
-
-    Returns:
-        {integer} -- 2 to the power of n
-    """
-    p = 1
-    while n > p:
-        p *= 2
-    return p
-
-# ----------------------------------------------------------------------------
-#
-def gaussian(sdev, size, value_type):
-
-    from math import exp
-    from numpy import empty, add, divide
-
-    g = empty((size,), value_type)
-    for i in range(size):
-        u = min(i,size-i) / sdev
-        p = min(u*u/2, 100)               # avoid OverflowError with exp()
-        g[i] = exp(-p)
-    area = add.reduce(g)
-    divide(g, area, g)
-    return g
-
-# ----------------------------------------------------------------------------
-#
-fast_fft_sizes = [2, 3, 4, 5, 6, 8, 9, 10, 12, 16, 18, 20, 21, 25, 32, 33, 36, 40, 48, 50, 54, 55, 64, 65, 72, 80, 81, 90, 96, 100, 108, 120, 128, 144, 150, 160, 162, 180, 192, 200, 216, 256, 270, 300, 320, 324, 325, 336, 360, 400, 432, 450, 480, 500, 512, 540, 576, 600, 640, 648, 720, 750, 800, 810, 864, 900, 960, 1024, 1080, 1152, 1200, 1280, 1296, 1350, 1440, 1500, 1536, 1600, 1620, 1728, 1800, 1920, 2048, 2160, 2250, 2304, 2400, 2560, 2700, 2880, 3000, 3072, 3200, 3240, 3600, 3840, 4096, 4160, 4176, 4180, 4186, 4187, 4192]
-def efficient_fft_size(n):
-
-    if n < fast_fft_sizes[-1]:
-        from bisect import bisect
-        b = bisect(fast_fft_sizes, n)
-        s = fast_fft_sizes[b]
-    else:
-        s = ceil_power_of_2(n)
-    return s
-
-# ----------------------------------------------------------------------------
-# Compute with zero padding in real-space to avoid cyclic-convolution.
-#
-def gaussian_convolution(data, ijk_sdev, value_type = None,
-                         cyclic = False, cutoff = 5, invert = False, task = None):
-
-    if value_type is None:
-        value_type = data.dtype
-      
-    from numpy import array, float32, float64, multiply, divide, swapaxes
-    vt = value_type if value_type == float32 or value_type == float64 else float32
-    c = array(data, vt)
-      
-    from numpy.fft import rfft, irfft
-    for axis in range(3):           # Transform one axis at a time.
-        size = c.shape[axis]
-        if size == 1:
-            continue          # For a plane don't try to filter normal to plane.
-        sdev = ijk_sdev[2-axis]       # Axes i,j,k are 2,1,0.
-        hw = min(size/2, int(cutoff*sdev+1)) if cutoff else size/2
-        nzeros = 0 if cyclic else hw  # Zero-fill for non-cyclic convolution.
-        if nzeros > 0:
-            # FFT performance is much better (up to 10x faster in numpy 1.2.1)
-            # than other sizes.
-            nzeros = efficient_fft_size(size + nzeros) - size
-        g = gaussian(sdev, size + nzeros, vt)
-        g[hw:-hw] = 0
-        fg = rfft(g)                  # Fourier transform of 1-d gaussian.
-        cs = swapaxes(c, axis, 2)     # Make axis 2 the FT axis.
-        s0 = cs.shape[0]
-        for p in range(s0):  # Transform one plane at a time.
-            cp = cs[p,...]
-            try:
-                ft = rfft(cp, n=len(g))   # Complex128 result, size n/2+1
-            except ValueError:
-                raise MemoryError     # Array dimensions too large.
-            if invert:
-                divide(ft, fg, ft)
-            else:
-                multiply(ft, fg, ft)
-            cp[:,:] = irfft(ft)[:,:size] # Float64 result
-            if task:
-                pct = 100.0 * (axis + float(p)/s0) / 3.0
-                task.updateStatus('%.0f%%' % pct)
-      
-    if value_type != vt:
-        return c.astype(value_type)
-      
-    return c
